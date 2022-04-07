ffc1f1f61b28002d0cb3a6fee5d7a0be964dd2b2
Strashok Yana <jane.strashok@gmail.com>
2022-04-06 21:48:39 -0500

    Initial Commit

---
+++ Lab2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab2
+//
+//  Created by Яна Страшок on 06.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Яна Страшок on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
